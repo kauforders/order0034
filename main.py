b9a1b3e63671b95f3fa98409fe02756f6c1c4b34
kauforders <149829652+kauforders@users.noreply.github.com>
2023-11-03 21:13:38 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+"""
+This example shows how to use webhook on behind of any reverse proxy (nginx, traefik, ingress etc.)
+"""
+import logging
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+from aiohttp import web
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+
+load_dotenv()
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+WEBHOOK_SECRET = getenv("WEBHOOK_SECRET")
+BASE_WEBHOOK_URL = getenv("BASE_WEBHOOK_URL")
+WEB_SERVER_HOST = getenv("WEB_SERVER_HOST")
+
+
+
+# Port for incoming request from reverse proxy. Should be any available port
+WEB_SERVER_PORT = 8443
+
+# Path to webhook route, on which Telegram will send requests
+WEBHOOK_PATH = "/webhook"
+
+# All handlers should be attached to the Router (or Dispatcher)
+router = Router()
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer("🔝 Main Menu")
+
+
+@router.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def on_startup(bot: Bot) -> None:
+    # If you have a self-signed SSL certificate, then you will need to send a public
+    # certificate to Telegram
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
+
+
+def main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Register startup hook to initialize webhook
+    dp.startup.register(on_startup)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+
+    # Create aiohttp.web.Application instance
+    app = web.Application()
+
+    # Create an instance of request handler,
+    # aiogram has few implementations for different cases of usage
+    # In this example we use SimpleRequestHandler which is designed to handle simple cases
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    # Register webhook handler on application
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    # Mount dispatcher startup and shutdown hooks to aiohttp application
+    setup_application(app, dp, bot=bot)
+
+    # And finally start webserver
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    main()
